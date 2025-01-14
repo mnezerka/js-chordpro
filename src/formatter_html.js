cf50ee52d8c0083be9c27fb4eda0d46b99c961f6
Michal Nezerka <michal.nezerka@gmail.com>
2025-01-14 20:46:08 +0100

    fixed html formatter

--- src/formatter_html.js
+++ src/formatter_html.js
@@ -1,11 +1,11 @@
 
-function processRow(node) {
+function processLine(line) {
     let chords = [];
     let lyrics = [];
     let last_chord = null;
 
-    for (let i = 0; i < row.children.length; i++) {
-        let item = row.children[i];
+    for (let i = 0; i < line.items.length; i++) {
+        let item = line.items[i];
 
         if (item.type === "chord") {
             chords.push(item.value);
@@ -16,24 +16,24 @@ function processRow(node) {
                 chords.push("")
             }
             lyrics.push(item.value);
-            last_chord = null 
+            last_chord = null
         }
     }
 
-    let result = '<table class="jschordpro-row">';
+    let result = '<table class="line">';
 
-    // chords row
-    // check if there is at least one real chord in a row
+    // chords line
+    // check if there is at least one real chord in a line
     let chords_real = chords.filter(c => c !== "")
     if (chords_real.length > 0) {
-        result += '<tr class="jschordpro-row-chords">';
-        result += chords.map(c => `<td class="jschordpro-chord">${c}</td>`).join("\n");
+        result += '<tr class="line-chords">';
+        result += chords.map(c => `<td class="chord">${c}</td>`).join("\n");
         result += '</tr>';
     }
 
-    // lyris row
-    result += '<tr class="jschordpro-row-lyrics">';
-    result += lyrics.map(l => `<td class="jschordpro-lyrics">${l.replaceAll(" ", "&nbsp;")}</td>`).join("\n");
+    // lyris line
+    result += '<tr class="line-lyrics">';
+    result += lyrics.map(l => `<td class="lyrics">${l.replaceAll(" ", "&nbsp;")}</td>`).join("\n");
     result += '</tr>';
 
     result += '</table>';
@@ -41,47 +41,39 @@ function processRow(node) {
     return result;
 }
 
-function processTextPart(part) {
-    let result = `<div class="jschordpro-${part.type}">`;
-    for (let i = 0; i < part.lines.length; i++) {
-        let row = part.lines[i];
-        if (row.type != "row") { continue }
-        result += process_row(row);
+function processContentBlock(block) {
+    let result = `<div class="${block.type}">\n`;
+    for (let i = 0; i < block.items.length; i++) {
+        let line = block.items[i];
+        result += processLine(line);
     }
     result += '</div>'; // verse
 
     return result;
 }
 
-function processSong(doc) {
-    var result = '<div class="jschordpro-song">';
+function processSong(song) {
+    var result = '<div class="jschordpro-song">\n\n';
 
-    if (doc.type === "chordpro") {
-        result += '<div class="jschordpro-header">';
-        if (doc.header.title.length > 0) { result += `<h1>${doc.header.title}</h1>` };
-        if (doc.header.subtitle.length > 0) { result += `<h2>${doc.header.subtitle}</h2>` };
-        if (doc.header.artist.length > 0) { result += `<h2>${doc.header.artist}</h2>` };
-        result += '</div>';
+    result += '<div class="header">\n';
+    if (song.header.title && song.header.title.length > 0) { result += `<h1>${song.header.title}</h1>\n` };
+    if (song.header.subtitle && song.header.subTitle.length > 0) { result += `<h2>${song.header.subTitle}</h2>\n` };
+    if (song.header.artist && song.header.artist.length > 0) { result += `<h2>${song.header.artist}</h2>\n` };
+    result += '</div>\n\n'; // header
 
-        // loop through song content item by item
-        if (doc.content.type === "content") {
-            result += '<div class="jschordpro-content">';
-            for (let i = 0; i < doc.content.parts.length; i++) {
-                let part = doc.content.parts[i];
-                result += process_text_part(part)
-            }
-            result += '</div>';
-        }
-
+    // loop through song content item by item
+    result += '<div class="content">\n';
+    for (let i = 0; i < song.content.length; i++) {
+        let block = song.content[i];
+        result += processContentBlock(block)
     }
+    result += '</div>\n'; // content
 
-    result += '</div>';
+    result += '</div>\n'; // song
 
     return result;
 }
 
 module.exports = {
     processSong,
-    processTextPart,
-    processRow
 }
