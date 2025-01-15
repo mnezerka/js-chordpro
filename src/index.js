d66eb6e5a6c2af92078e24c7ae693005569a3ca0
Michal Nezerka <michal.nezerka@gmail.com>
2025-01-15 22:54:50 +0100

    export transpose utility

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const Grammar = require('./grammar.js')
 const FormatterAscii = require("./formatterAscii.js");
 const FormatterHtml = require("./formatterHtml.js");
+const utils = require('./utils.js');
 
 module.exports.parse = Grammar.parse;
 module.exports.to_ascii = FormatterAscii.processSong;
 module.exports.to_html = FormatterHtml.processSong;
+module.exports.transpose = utils.transpose;
+
